c488f121570f55d46498f14adf92d347ef0a178a
roddutra <rodrigofd87@gmail.com>
2023-02-05 16:09:46 +1000

    Fix incorrect navbar import

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 'use client';
 import { Footer } from '../components/Footer';
-import { Navbar } from '../components/navbar';
+import { Navbar } from '../components/Navbar';
 import '../styles/globals.css';
 export default function RootLayout({ children }) {
   return (
