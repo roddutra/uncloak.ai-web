4c689b0168edd5527543e10bcd596fba8f7d49ff
roddutra <rodrigofd87@gmail.com>
2023-02-05 16:38:06 +1000

    Remove auth pages

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,4 @@
-import { Button } from './Button';
 import { Container } from './Container';
-import { TextField } from './Fields';
 import { NavLinks } from './NavLinks';
 
 export function Footer() {
@@ -23,20 +21,6 @@ export function Footer() {
           </div>
         </div>
         <div className='flex flex-col items-start border-t border-gray-200 pt-8 pb-12 md:justify-between md:pt-6'>
-          {/* <form className='flex w-full justify-center md:w-auto'>
-            <TextField
-              type='email'
-              aria-label='Email address'
-              placeholder='Email address'
-              autoComplete='email'
-              required
-              className='w-60 min-w-0 shrink'
-            />
-            <Button type='submit' color='cyan' className='ml-4 flex-none'>
-              <span className='hidden lg:inline'>Join our newsletter</span>
-              <span className='lg:hidden'>Join newsletter</span>
-            </Button>
-          </form> */}
           <p className='mt-6 text-sm text-gray-500 md:mt-0'>
             &copy; Copyright {new Date().getFullYear()}. All rights reserved.
           </p>
