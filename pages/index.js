d400d8e69ffbdf884d5464b64470db8ac4a9a02d
roddutra <rodrigofd87@gmail.com>
2023-02-21 20:27:30 +1000

    Fix vercel deploy error

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import AskQuestionDemo from '../components/AskQuestionDemo';
 import Faqs from '../components/Faqs';
 import Hero from '../components/Hero';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import { siteName } from '../lib/constants';
 
 export default function Index() {
